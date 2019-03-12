6cc499990f0ad8bb703e759a396c45817977ed21
Minuk Kim <mkim01@stuy.edu>
2019-03-12 11:56:19 -0400

    partition function in class

---
+++ Quick.java
@@ -0,0 +1,55 @@
+public class Quick{
+
+  /*Modify the array such that:
+ *1. Only the indices from start to end inclusive are considered in range
+ *2. A random index from start to end inclusive is chosen, the corresponding
+ *   element is designated the pivot element.
+ *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
+ *4. all elements in range that are larger than the pivot element are placed after the pivot element.
+ *@return the index of the final position of the pivot element.
+ */
+
+  public static String partition(int[] data, int start, int end){
+    int pindex = (int)(Math.random() * (end - start)) + start;
+    int pivot = data[pindex];
+    data[pindex] = data[start];
+    data[start] = pivot;
+    pindex = start;
+    // set pivot index to the beginning and loop through the array
+    while (start != end){
+      if (data[start] > pivot){
+        int holder = data[start];
+        data[start] = data[end];
+        data[end] = holder;
+        end --;
+      }
+      else{
+        start++;
+      }
+    }
+    //debug
+
+    
+    String output = "";
+    output += pivot + "|";
+    for(int i = 0; i < data.length; i++){
+      output += data[i] + " ";
+    }
+
+    return output;
+  }
+
+
+
+
+
+  public static int quickselect(int[] data, int k){
+    //int pivot = partition(data,0, data.length - 1);
+    return 0;
+  }
+
+  public static void main(String[] args){
+    int[] ary = {2,10,15,23,0,5};
+    System.out.println(partition(ary,2,2));
+  }
+}
