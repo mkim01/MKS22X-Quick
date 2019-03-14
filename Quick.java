3f7d6b4e8728f09d027dd4c85628913d4ea469fd
Minuk Kim <mkim01@stuy.edu>
2019-03-14 12:47:44 -0400

    tested and working partition

--- Quick.java
+++ Quick.java
@@ -9,12 +9,13 @@ public class Quick{
  *@return the index of the final position of the pivot element.
  */
 
-  public static String partition(int[] data, int start, int end){
+  public static int partition(int[] data, int start, int end){
     int pindex = (int)(Math.random() * (end - start)) + start;
     int pivot = data[pindex];
     data[pindex] = data[start];
     data[start] = pivot;
     pindex = start;
+  //  start++;
     // set pivot index to the beginning and loop through the array
     while (start != end){
       if (data[start] > pivot){
@@ -27,29 +28,45 @@ public class Quick{
         start++;
       }
     }
+
+    if (data[start] > pivot){
+      data[pindex] = data[start - 1];
+      data[start = 1] = pivot;
+      pindex = start - 1;
+    }
+    else{
+      data[pindex] = data[start];
+      data[start] = pivot;
+      pindex = start;
+    }
+    // return pindex;
+
     //debug
 
-    
-    String output = "";
-    output += pivot + "|";
-    for(int i = 0; i < data.length; i++){
-      output += data[i] + " ";
-    }
+    // String output = "";
+    // output += pivot + "|";
+    // for(int i = 0; i < data.length; i++){
+    //   output += data[i] + " ";
+    // }
+    return pindex;
 
-    return output;
   }
-
 
 
 
 
   public static int quickselect(int[] data, int k){
     //int pivot = partition(data,0, data.length - 1);
+    // int h = partition(data, 0, end);
+    // if (h > k) {
+    //   end = h - 1;
+    // }
+
     return 0;
   }
 
   public static void main(String[] args){
     int[] ary = {2,10,15,23,0,5};
-    System.out.println(partition(ary,2,2));
+    System.out.println(partition(ary,0,5));
   }
 }
